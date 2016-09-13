1f2dd58a920ec4b164b570bea1be8877921082b0
Mili Ayyar <Mili Ayyar>
2016-09-13 23:31:46 +0530

    Final implementation

--- Anagram Game/app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
+++ Anagram Game/app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -15,10 +15,12 @@ public class AnagramDictionary {
     private static final int MIN_NUM_ANAGRAMS = 5;
     private static final int DEFAULT_WORD_LENGTH = 3;
     private static final int MAX_WORD_LENGTH = 7;
+    private static int wordLength = DEFAULT_WORD_LENGTH;
     private Random random = new Random();
     private ArrayList wordList = new ArrayList();
     private HashSet<String> wordSet = new HashSet<String>();
     private HashMap<String , ArrayList<String>> lettersToWord = new HashMap<String , ArrayList<String> >();
+    private HashMap<Integer , ArrayList<String>> sizeToWords = new HashMap<Integer , ArrayList<String>>();
 
     //Constructor
     public AnagramDictionary(InputStream wordListStream) throws IOException {
@@ -38,11 +40,24 @@ public class AnagramDictionary {
             if(lettersToWord.containsKey(sortWord)){
                 temp_word = lettersToWord.get(sortWord);
                 temp_word.add(word);
+                lettersToWord.put(sortWord,temp_word);
             }
 
             else {
                 temp_word.add(word);
                 lettersToWord.put(sortWord,temp_word);
+            }
+            ArrayList<String> temp_word1 = new ArrayList<String>();
+
+            //Creating Hash Map Mapping words to their lengths
+            if(sizeToWords.containsKey(word.length())){
+                temp_word1 = sizeToWords.get(word.length());
+                temp_word1.add(word);
+                sizeToWords.put(word.length(),temp_word1);
+            }
+            else {
+                temp_word1.add(word);
+                sizeToWords.put(word.length(),temp_word1);
             }
 
         }
@@ -64,11 +79,11 @@ public class AnagramDictionary {
             return false;
     }
 
-    public ArrayList<String> getAnagrams(String targetWord) {
-        ArrayList<String> result = new ArrayList<String>();
+    //public ArrayList<String> getAnagrams(String targetWord) {
+       // ArrayList<String> result = new ArrayList<String>();
 
-        return result;
-    }
+     //   return result;
+   // }
 
     public ArrayList<String> getAnagramsWithOneMoreLetter(String word) {
         ArrayList<String> result = new ArrayList<String>();
@@ -82,9 +97,9 @@ public class AnagramDictionary {
             }
         }
         for (int i =0 ; i<result.size();i++){
-            Log.d("AD list",result.get(i));
+            //Log.d("AD list",result.get(i));
             if(!isGoodWord(result.get(i),word)){
-                Log.d("AD list" , result.remove(i));
+                //Log.d("AD list" , result.remove(i));
                 result.remove(i);
             }
 
@@ -94,6 +109,34 @@ public class AnagramDictionary {
     }
 
     public String pickGoodStarterWord() {
-        return "stop";
+        String word = new String();
+        int j;
+        ArrayList<String> lengthWords = new ArrayList<String>();
+
+        if(wordLength<= MAX_WORD_LENGTH){
+            lengthWords = sizeToWords.get(wordLength);
+        }
+        int i = random.nextInt(lengthWords.size());
+        for (j=i;j<lengthWords.size();j++){
+            if(getAnagramsWithOneMoreLetter(lengthWords.get(j)).size()>= MIN_NUM_ANAGRAMS){
+                word = lengthWords.get(j);
+                break;
+            }
+
+        }
+        //Keep While in place of for and just add a counter
+        if(j==lengthWords.size()-1 || word== null){
+            for (j=0;j<i;j++){
+                if(getAnagramsWithOneMoreLetter(lengthWords.get(j)).size()>= MIN_NUM_ANAGRAMS){
+                    word = lengthWords.get(j);
+                    break;
+                }
+
+            }
+        }
+        if (wordLength <MAX_WORD_LENGTH){
+            wordLength++;
+        }
+        return word;
     }
 }
